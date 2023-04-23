e331469578591c1e26460fc5304137c212094296
JangChangKyoon <58103130+JangChangKyoon@users.noreply.github.com>
2023-04-23 21:47:44 +0900

    Search delicately

--- pages/api/tweets/search/[terms].ts
+++ pages/api/tweets/search/[terms].ts
@@ -7,10 +7,19 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { terms } = req.query;
   // url에서 검색어 가져오기
 
+  //검색어를 띄어쓰기 단위로 끊어서 리스트로 만들기(리스트의 각 요소별 검색 위해)
+  const termsArr = String(terms)
+    .split(" ")
+    .map((word) => ({
+      text: {
+        contains: word,
+      },
+    }));
+
   // url에서 가져온 검색어로 데이터베이스 조회하여 데이터 가져오기
   const search = await client.tweet.findMany({
     where: {
-      text: terms + "",
+      OR: termsArr,
     },
   });
 
